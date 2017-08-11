427bf2ca0688f3840a21401eb510b405217cac29
Théo Attali <theoattali@gmail.com>
2017-08-11 17:29:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# electron twig
+This module is a simple `file` protocol interceptor for [electron](https://github.com/atom/electron) which compiles all (local) URLs to files with `.twig` extension.
+
+It's inspired by [yan-foto/electron-pug](https://github.com/yan-foto/electron-pug)
+
+<!--# Installation
+
+```
+npm install electron-twig
+```
+
+# Usage
+Just initialize this module with [Twig](https://www.npmjs.com/package/twig) package trougth `electron-twig`:
+
+```js
+const {app, BrowserWindow} = require('electron')
+const twig                 = require('electron-twig')
+
+app.on('ready', () => {
+  let win = new BrowserWindow({width: 800, height: 600})
+
+  win.loadURL(`file://${__dirname}/view/index.html.twig`)
+
+  // etc.
+})
+```
+-->
---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const {app, protocol} = require('electron')
+const path            = require('path')
+const fs              = require('fs')
+const twig            = require('twig')
+const mime            = require('mime')
+
+
+const getPath = url => {
+  let parsed = require('url').parse(url);
+  let result = decodeURIComponent(parsed.pathname);
+
+  // Local files in windows start with slash if no host is given
+  // file:///c:/something.pug
+  if(process.platform === 'win32' && !parsed.host.trim()) {
+    result = result.substr(1);
+  }
+
+  return result;
+}
+
+const interceptCB = error => {
+  if (!error) {
+    console.log('Twig interceptor registered successfully');
+  } else {
+    console.error('Twig interceptor failed:', error);
+  }
+}
+
+app.on('ready', () => {
+  protocol.interceptBufferProtocol('file', (request, callback) => {
+    let file = getPath(request.url);
+
+    // See if file actually exists
+    try {
+      let content = fs.readFileSync(file);
+
+      let ext = path.extname(file);
+      if (ext === '.twig') {
+        twig.renderFile(file, {}, (err, html) => {
+          return callback({data: new Buffer(html), mimeType:'text/html'});
+        })
+      } else {
+        return callback({data: content, mimeType: mime.lookup(ext)});
+      }
+    } catch (e) {
+      // All error wrt. Twig are rendered in browser
+      if (e.code.startsWith('TWIG:')) {
+        return callback({data: new Buffer(`<pre style="tab-size:1">${e}</pre>`), mimeType:'text/html'});
+      }
+
+      // See here for error numbers:
+      // https://code.google.com/p/chromium/codesearch#chromium/src/net/base/net_error_list.h
+      if (e.code === 'ENOENT') {
+       // NET_ERROR(FILE_NOT_FOUND, -6)
+       return callback(6);
+      }
+
+      // All other possible errors return a generic failure
+      // NET_ERROR(FAILED, -2)
+      return callback(2);
+    }
+  }, (error) => {
+    if (!error) console.error('Failed to register protocol')
+  })
+})
