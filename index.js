557ece730efa25c834f98aea84f5f1bd255ee6dd
aurelien_colombet <aurelien@eludice.com>
2019-04-03 17:55:07 +0200

    Enhanced async support

--- index.js
+++ index.js
@@ -34,20 +34,21 @@ app.on('ready', () => {
 
       let ext = path.extname(file);
       if (ext === '.twig') {
+
         if(twigOptions !== undefined && twigOptions.is_async)
         {
-            let contentAsync = fs.readFileSync(file, "utf8");
-            twig.twig({allowInlineIncludes: true, data: contentAsync}).renderAsync(twigOptions).then(function(output) {
-                return callback({data: new Buffer(output), mimeType:'text/html'});
-            });
+            twigOptions.settings = [];
+            twigOptions.settings['twig options'] = {};
+            twigOptions.settings['twig options'].allow_async = true;
         }
-        else
-        {
-            twig.renderFile(file, twigOptions, (err, html) => {
-                return callback({data: new Buffer(html), mimeType:'text/html'});
-            })
-        }
-      } else {
+
+        twig.renderFile(file, twigOptions, (err, html) => {
+            return callback({data: new Buffer(html), mimeType:'text/html'});
+        });
+
+      }
+      else
+      {
         return callback({data: content, mimeType: mime.lookup(ext)});
       }
     } catch (e) {
